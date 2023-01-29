c3db19321b116ed01247cd18ef1afdf13f6751da
Fedir Katushonok <fedir.katushonok@gmail.com>
2023-01-29 21:20:04 +0200

    initial

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by fedir on 1/29/23.
+//
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(){
+
+    string tracker_type = "MIL";
+
+    waitKey(0);
+
+    return 0;
+}
