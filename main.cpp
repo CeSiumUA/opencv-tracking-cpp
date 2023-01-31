302b749c1cd2e93c1d673c58696d5cd61c2c60a2
Fedir Katushonok <fedir.katushonok@gmail.com>
2023-01-31 05:51:10 +0200

    Updated tracknig process

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-//
-// Created by fedir on 1/29/23.
-//
-
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
@@ -25,14 +21,32 @@ int main(){
     Mat frame;
     bool ok = video.read(frame);
 
-    while(video.read(frame)){
-        double start_time = (double)getTickCount();
+    Rect roi;
 
-        putText(frame, "Waiting for an object", Point(20, 80), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 2);
+    cout << "Press an ESC key to exit" << endl;
+
+    while(video.read(frame)){
+
+        double start_time = (double)getTickCount();
 
         putText(frame, "Original FPS: " + static_cast<std::ostringstream &>(std::ostringstream() << std::dec << int(original_fps)).str(), Point(20, 50), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 2);
 
         putText(frame, "Alg: MIL", Point(20, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 2);
+
+        bool tracked = false;
+
+        if(!roi.empty()){
+            tracked = tracker -> update(frame, roi);
+            rectangle(frame, roi, Scalar(255, 0, 0), 2, 1);
+        }
+
+        string tracking_text = "Waiting for an object";
+
+        if(tracked){
+            tracking_text = "Target detected";
+        }
+
+        putText(frame, tracking_text, Point(20, 80), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 2);
 
         imshow("Tracking", frame);
 
@@ -45,12 +59,19 @@ int main(){
         }
 
         int k = waitKey(delay);
+
+        if(k != -1){
+            cout << "key pressed: " << to_string(k) << endl;
+        }
+
         if(k == 27){
             break;
         }
+        else if(k == 115){
+            roi = selectROI("Tracking", frame);
+            tracker -> init(frame, roi);
+        }
     }
-
-    waitKey(0);
 
     destroyAllWindows();
 
